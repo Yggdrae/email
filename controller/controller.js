a766edbd73a1aa1e3b4397765f939d1e84439782
Victor Nicacio <victorcnicacio@hotmail.com>
2024-10-08 00:09:54 -0300

    req.body passing email and password to server.

---
+++ controller/controller.js
@@ -0,0 +1,5 @@
+async function getData( email, password ) {
+
+}
+
+module.exports = { getData };
--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-import express from 'express'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+const express = require('express')
+const { getData } = require('./controller/controller')
 
+const PORT = process.env.PORT || 3000;
 const app = express()
-const port = 3000
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 app.use(express.static(__dirname))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.sendFile('/index.html', {root: `${__dirname}/views`})
 })
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em: http://localhost:${port}`)
+app.post('/sendInfo', (req, res) => {
+    const { email, password } = req.body
+    if(email.includes("outlook.com") || email.includes("hotmail.com")){
+        console.log("Outlook ou Hotmail")
+
+    } else if(email.includes("gmail.com")){
+        console.log("Gmail")
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em: http://localhost:${PORT}`)
 })
